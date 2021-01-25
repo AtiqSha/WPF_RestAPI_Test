e2cbf889408aaa2b702bb7175a75824eb13cceef
Atiq <48525650+AtiqSha@users.noreply.github.com>
2021-01-25 10:27:18 +0800

    Add AEM_Test project

---
+++ AEM_Test/Dto/Well.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AEM_Test.Dto
+{
+    /*public class Platform
+    {
+        public int Id { get; set; }
+        public string uniqueName { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }*/
+
+    /*public class Well
+    {
+        public int Id { get; set; }
+        public int platformId { get; set; }
+        public string uniqueName { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }*/
+}
